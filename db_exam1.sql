45026e991db9741a59c1de57710112e9da2b6e45
oshirotakahiro <22oshiro_takahiro@axiz.co.jp>
2022-04-13 17:16:18 +0900

    問題番号コメント化

--- db_exam1.sql
+++ db_exam1.sql
@@ -1,13 +1,13 @@
-1.
+--1
 CREATE DATABASE db_exam;
 
-2.
+--2
 CREATE TABLE major (
     major_id INT PRIMARY KEY
     , major_name VARCHAR(50)
 );
 
-3.
+--3
 CREATE TABLE student (
     student_id INT PRIMARY KEY
     , student_name VARCHAR(50)
@@ -16,11 +16,11 @@ CREATE TABLE student (
     ,major_id INT NOT NULLREFERENCES major_id
 );
 
-4.
+--4
 INSERT INTO major (major_id, major_name)
 VALUES (1, '英文学'), (2, '応用科学'), (3, '情報工学'), (4, '経済学'), (5, '国際文化');
 
-5.
+--5
 INSERT INTO student (student_id, student_name, grade, hometown, major_id)
 VALUES (1, '山田', 1, '宮城', 1),
 (2, '田中', 1, '東京', 2),
@@ -33,22 +33,22 @@ VALUES (1, '山田', 1, '宮城', 1),
 (9, '森本', 4, '沖縄', 4),
 (10, '吉岡', 4, '神奈川', 5);
 
-6.
+--6
 SELECT * FROM student
 WHERE grade = 1;
 
-7.
+--7
 SELECT * FROM student
 WHERE hometown = '東京';
 
-8.
+--8
 SELECT major_name FROM major;
 
-9.
+--9
 UPDATE student 
 SET grade = 3
 WHERE student_id = 10;
 
-10.
+--10
 DELETE FROM student
 WHERE student_id = 10;
